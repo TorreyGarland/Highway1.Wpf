de311f00536f41b1d94d21cc4eb11f3cf6e58790
crazytmack <crazytmack@TELETRON1A>
2016-08-12 23:40:53 -0400

    Implementing "ICommand" interface for "DelegateCommandBase" class.

--- Highway1.Wpf/DelegateCommandBase.cs
+++ Highway1.Wpf/DelegateCommandBase.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Highway1.Wpf
 {
-    public abstract class DelegateCommandBase
+    public abstract class DelegateCommandBase : ICommand
     {
     }
 }
