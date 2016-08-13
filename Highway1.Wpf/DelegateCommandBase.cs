0f618c3dce537f4a78f32589e53ab962713619e4
crazytmack <crazytmack@TELETRON1A>
2016-08-12 23:35:54 -0400

    Adding "DelegateCommandBase" class.

---
+++ Highway1.Wpf/DelegateCommandBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highway1.Wpf
+{
+    public abstract class DelegateCommandBase
+    {
+    }
+}
