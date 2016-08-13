8e56c781a67bc7e17a3e431e46c32d0e8dd3bfcd
crazytmack <crazytmack@TELETRON1A>
2016-08-12 23:45:17 -0400

    Implementation completed.

--- Highway1.Wpf/DelegateCommandBase.cs
+++ Highway1.Wpf/DelegateCommandBase.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-namespace Highway1.Wpf
+﻿namespace Highway1.Wpf
 {
+
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Input;
+
     public abstract class DelegateCommandBase : ICommand
     {
+
+        #region Properties
+
+        protected Func<object, bool> Predicate { get; }
+
+        #endregion
+
+        #region Methods
+
+        [DebuggerStepThrough]
+        protected DelegateCommandBase(Func<object, bool> predicate)
+        {
+            Predicate = predicate;
+        }
+
+        [DebuggerStepThrough]
+        public virtual bool CanExecute(object parameter)
+            => (Predicate?.Invoke(parameter)).GetValueOrDefault(true);
+
+        public abstract void Execute(object parameter);
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #endregion
+
     }
+
 }
